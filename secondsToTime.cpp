309dff28882e8b69191cfed25db554ba79b49f35
Tony Shelton-McGaha <60279886+ashelto6@users.noreply.github.com>
2023-03-22 00:09:38 -0600

    Update secondsToTime.cpp

--- secondsToTime.cpp
+++ secondsToTime.cpp
@@ -4,7 +4,7 @@
 
 std::string secondsToTime(int seconds)
 {
-/* GETTING HOURS */
+    /* GETTING HOURS */
     double hrsMinsSecs = (float)seconds/3600;
     int hr = hrsMinsSecs;
     int whole_hr = hr;
@@ -21,21 +21,21 @@ std::string secondsToTime(int seconds)
     if (hr < 10) { str_hr = "0" + std::to_string(hr); }
     else { str_hr = std::to_string(hr); }
 
-/* GETTING MINUTES */
+    /* GETTING MINUTES */
     double minsSecs = (hrsMinsSecs - whole_hr) * 60;
     int mins = minsSecs;
     if (mins < 10) { str_mins = "0" + std::to_string(mins); }
     else { str_mins = std::to_string(mins); }
 
-/* GETTING SECONDS */
+    /* GETTING SECONDS */
     int secs = round(((minsSecs - mins) * 60));
     if (secs < 10) { str_secs = "0" + std::to_string(secs); }
     else { str_secs = std::to_string(secs); }
 
-/* CREATE TIME STRING */
+    /* CREATE TIME STRING */
     std::string time = str_hr + ":" + str_mins + ":" + str_secs;
 
-/* RETURN */
+    /* RETURN */
     return time;
 }
 
